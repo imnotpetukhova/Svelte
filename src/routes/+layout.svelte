5b10d4a6997bc9dbcc6069803d54eed2257bedab
Jenny <espetuhova@icloud.com>
2023-05-30 17:49:08 +0300

    add todo app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+            {#if link.isShow}
+            <a href={link.href} class="link">{link.title}</a>
+            {/if}
+            {/each}
         </div>
     </div>
 </nav>
@@ -14,6 +15,36 @@
     <!-- Pages will be injected below -->
     <slot></slot>
 </div>
+
+<script>
+    const nav = [
+    {
+      title: 'Обо мне',
+      href: '/about',
+      isShow: true
+    },
+    {
+      title: 'Блог',
+      href: '/blog',
+      isShow: true
+    },
+    {
+      title: 'Портфолио',
+      href: '/project',
+      isShow: true
+    },
+    {
+      title: 'Контакты',
+      href: '/contact',
+      isShow: true
+    },
+    {
+      title: 'temp',
+      href: '/temp',
+      isShow: false
+    }
+  ]
+</script>
 
 <style>
     .container {
